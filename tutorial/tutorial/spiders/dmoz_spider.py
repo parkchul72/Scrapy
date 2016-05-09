de563a2a8100d1843fc9bb5e8d7cfdff9e0d6f6b
Fred <parkchul@nate.com>
2016-05-09 17:41:30 +0900

    20160509 KOBIS V0.7

--- tutorial/tutorial/spiders/dmoz_spider.py
+++ tutorial/tutorial/spiders/dmoz_spider.py
@@ -4,11 +4,34 @@ from tutorial.items import DmozItem
 class DmozSpider(scrapy.Spider):
     name = "dmoz"
     allowed_domains = ["dmoz.org"]
+    """
+    For #.1 ~ 3
     start_urls = [
         "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
         "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
     ]
+    """
+    """
+    For #.4
+    """
+    start_urls = [
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/",
+    ]
 
+    def parse(self, response):
+        for href in response.css("ul.directory.dir-col > li > a::attr('href')"):
+            url = response.urljoin(href.extract())
+            yield scrapy.Request(url, callback=self.parse_dir_contents)
+
+    def parse_dir_contents(self, response):
+        for sel in response.xpath('//ul/li'):
+            item = DmozItem()
+            item['title'] = sel.xpath('a/text()').extract()
+            item['link'] = sel.xpath('a/@href').extract()
+            item['desc'] = sel.xpath('text()').extract()
+            yield item
+
+"""
     #.3
     def parse(self,response):
         for sel in response.xpath('//ul/li'):
@@ -18,7 +41,6 @@ class DmozSpider(scrapy.Spider):
             item['desc'] = sel.xpath('text()').extract()
             yield item
 
-"""
     #.2
     def parse(self, reponse):
         for sel in reponse.xpath('//ul/li'):
--- tutorial/tutorial/spiders/kobis.py
+++ tutorial/tutorial/spiders/kobis.py
@@ -1,16 +1,28 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import logging
 from tutorial.items import KobisItem
+
+logging.basicConfig(level=logging.DEBUG)
 
 class KobisSpider(scrapy.Spider):
     name = "kobis"
-    allowed_domains = ["kobis.org"]
+    allowed_domains = ["kobis.or.kr"]
     start_urls = [
         "http://www.kobis.or.kr/kobis/business/mast/mvie/searchMovieList.do"
     ]
-    # Paging처리 요망 http://www.kobis.or.kr/kobis/business/mast/mvie/searchMovieList.do&curPage=
+    # Paging처리 요망 http://www.kobis.or.kr/kobis/business/mast/mvie/searchMovieList.do?curPage=
 
     def parse(self, response):
+        search_cnt = int(response.xpath('//div[@class="board_top02"]/em').re(':\s+([0-9]+)')[0])
+        page = search_cnt / 10
+        if (search_cnt % 10) > 0: page += 1
+
+        for next_page_no in range(1, page+1):
+            url = response.urljoin("?curPage=" + str(next_page_no))
+            yield scrapy.Request(url, callback=self.parse_next_page)
+
+    def parse_next_page(self, response):
         for sel in response.xpath('//table[@class="boardList03"]/tbody/tr'):
             title_list = sel.xpath('td/@title').extract()
             item = KobisItem()
